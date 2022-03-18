6d29b15384e3904dbbb5b0bd3d7c67c5bceb9341
YHSong <yhsong@dsti.co.kr>
2022-03-18 17:14:23 +0900

    localStorage

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+// variable declaration
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit() {
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// function define
+function onSubmit(event) {
+   event.preventDefault();
+
    const username = loginInput.value;
-   console.log(username); 
+
+   localStorage.setItem(USERNAME_KEY, username);
+
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   greeting.innerText = "Hello, " + username;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// function handleLinkClick(event) {
+//    event.preventDefault();
+//    console.log(event);
+// }
+
+// link.addEventListener("click", handleLinkClick);
+
+if (savedUsername === null) {
+   loginForm.addEventListener("submit", onSubmit);
+} else {
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   greeting.innerText = "Hello, " + savedUsername;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
