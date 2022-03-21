26541ccb2c3678f0d9a7c308fef87ba777736f0a
songsong <dragonh0919@gmail.com>
2022-03-22 00:19:25 +0900

    clock

--- js/clock.js
+++ js/clock.js
@@ -1,19 +1,29 @@
-let clock = document.querySelector("#clock");
+const clock = document.querySelector("#clock");
 
+// 현재 시간 받아와서 HH:MM:SS 로 변환
 function getClock() {
     const date = new Date();
     console.log(date);
 
-    console.log(typeof(date.getSeconds));
+    const hour = convertNum(date.getHours());
+    const minute = convertNum(date.getMinutes());
+    const second = convertNum(date.getSeconds());
 
-    clock.innerHTML = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    clock.innerHTML = hour + ':' + minute + ':' + second;
 }
 
-function convertSecond(seconds) {
-    if (seconds < '10') {
-        
+// 한자리수 시간 앞에 0 붙이기 방법 1
+function convertNum(time) {
+    if (time < '10') {
+        time = '0' + time;
     }
+    return time;
+}
+// 방법 2
+function sliceNum(time) {
+    return ('0' + time).slice(-2);
 }
 
+getClock();
 setInterval(getClock, 1000);
 //setTimeout(getClock, 2000);
